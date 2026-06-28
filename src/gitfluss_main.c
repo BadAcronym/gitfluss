e1142b3c840963e76423a95340f54f07726fc655
BadAcronym <terribleacronym@gmail.com>
2026-06-28 18:21:26 +0200

    add puddle + gitignore, count commits in repositories

--- src/gitfluss_main.c
+++ src/gitfluss_main.c
@@ -11,45 +11,53 @@ int main
     void
 ){
     // TODO: read these from some config file
-    const char *repo_name = "/home/mandi/repository/river2D_mapedit";
-    const char *authors   = "test@123.com;terribleacronym@gmail.com";
-    uint8_t     actions   = 0; // enum int with like COMMITS_ONLY, COMMITS_MERGES, etc etc
+    StringView repositories = cstr_sv("/home/mandi/repository/river2D_mapedit;/home/mandi/repository/river2D");
+    StringView authors      = cstr_sv("test@123.com;terribleacronym@gmail.com");
+    uint8_t    actions      = 0; // enum int with like COMMITS_ONLY, COMMITS_MERGES, etc
 
-    git_repository *repo;
-    git_revwalk    *revwalk;
-    git_oid        oid;
+    uint32_t repository_count = sv_count_by_delim(repositories, ';');
 
     git_libgit2_init();
 
-    // for(;;) <- for each repository
+    for(uint32_t i = 0; i < repository_count; ++i)
     {
-        uint32_t commit_count = 0;
+        git_repository *repo    = 0;
+        git_revwalk    *revwalk = 0;
+        git_oid        oid      = {0};
 
-        git_repository_open(&repo, repo_name);
+        StringView repository   = sv_find_by_delim(repositories, ';', i);
+        uint32_t   commit_count = 0;
+
+        printf("\nAnalyzing repository %u: "PRI_SV"\n", i, ARG_SV(repository));
+
+        const char *current_repo_cstr = sv_cstr(repository);
+
+        git_repository_open(&repo, current_repo_cstr);
         git_revwalk_new(&revwalk, repo);
         git_revwalk_push_head(revwalk);
 
         while(!git_revwalk_next(&oid, revwalk))
         {
-            git_commit *commit;
+            git_commit *commit = 0;
             git_commit_lookup(&commit, repo, &oid);
 
             const git_signature *sign = git_commit_author(commit);
 
-            if(sign->email != authors)
-            {
-            }
+            StringView author_mail = cstr_sv(sign->email);
 
-            ++commit_count;
+            if(sv_find(author_mail, authors))
+            {
+                ++commit_count;
+            }
 
             git_commit_free(commit);
         }
-        printf("commits in %s: %i\n", repo_name, commit_count);
+        printf("commits in "PRI_SV": %i\n", ARG_SV(repository), commit_count);
+
+        git_revwalk_free(revwalk);
+        git_repository_free(repo);
+        free((void*)current_repo_cstr);
     }
 
-    git_revwalk_free(revwalk);
-    git_repository_free(repo);
-    git_libgit2_shutdown();
-
-    return 0;
+    return git_libgit2_shutdown();
 }
