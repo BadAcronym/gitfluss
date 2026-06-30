e5c9c5678298f8c022a79683610a9f02798e8a91
BadAcronym <terribleacronym@gmail.com>
2026-06-30 08:55:49 +0200

    include time.h

--- include/gitfluss.h
+++ include/gitfluss.h
@@ -2,7 +2,16 @@
 
 #include "string_view.h"
 
+#ifdef BUILD_LINUX
+    #define __USE_POSIX199309
+    #include <time.h>
+#endif
 #include <stdint.h>
+
+#define GF_TYPE_ERROR     0
+#define GF_TYPE_FILE      1
+#define GF_TYPE_DIRECTORY 2
+#define GF_TYPE_OTHER     3
 
 uint64_t gfQueryTime
 (
