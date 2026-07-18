d911c62bbd1b5538a59ad21d670e06a65352f0be
badacronym <terribleacronym@gmail.com>
2026-07-18 22:07:21 +0200

    run script sketch

---
+++ run.ps1
@@ -0,0 +1,56 @@
+param($build)
+
+if(-Not(Test-Path "./obj/"))
+{
+    mkdir "./obj/"    
+}
+
+if(-Not(Test-Path "./build/"))
+{
+    mkdir "./build/"    
+}
+
+if(-Not(Test-Path "./bin/"))
+{
+    mkdir "./bin/"    
+}
+
+if($build -eq $null)
+{
+    $build = "release"
+}
+
+if($build -eq "asan" -or $build -eq "debug" -or $build -eq "release")
+{
+    Write-Host ""
+    Write-Host "compiling gitfluss..." -ForegroundColor Cyan
+    Write-Host ""
+    premake5 vs2026
+
+#     translate this
+#     pushd ./build/ > /dev/null
+#     make config=$build\_linux
+}
+else
+{
+     Write-Host "ERROR: invalid make config: '$build'." -ForegroundColor Red
+     exit -2;
+}
+
+# if [[ $? != 0 ]]
+# then
+#     echo -e "\033[31m\nERROR: failed to compile gitfluss.\n\033[0m"
+#     exit -1;
+# fi
+# 
+# echo -e "\n"
+# 
+# if [[ $2 = "--compile-only" ]]
+# then
+#     exit 0;
+# fi
+# 
+# popd > /dev/null
+# 
+# chmod +x ./bin/$build/gitfluss
+# ./bin/$build/gitfluss
